7538cbd07949838b7a83a17249e65c350782ce18
datnguyen199 <nnd.nguyennhudat@gmail.com>
2021-10-24 11:44:12 +0700

    add send location

--- src/index.js
+++ src/index.js
@@ -1,29 +1,41 @@
-const path = require('path')
-const express = require('express')
-const http = require('http')
-const socketio = require('socket.io')
+const path = require('path');
+const express = require('express');
+const http = require('http');
+const socketio = require('socket.io');
+const Filter = require('bad-words');
 
-const app = express()
-const server = http.createServer(app)
-const io = socketio(server)
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
-const port = process.env.PORT || 3000
-const publicDirectoryPath = path.join(__dirname, '../public')
+const port = process.env.PORT || 3000;
+const publicDirectoryPath = path.join(__dirname, '../public');
 
-app.use(express.static(publicDirectoryPath))
+app.use(express.static(publicDirectoryPath));
 
 io.on('connection', (socket) => {
   socket.broadcast.emit('message', 'a user just joined!');
-  socket.on('message chat', (msg) => {
-    // socket.broadcast.emit('message chat', msg);
+  socket.on('message chat', (msg, callback) => {
+    const filter = new Filter();
+    if(filter.isProfane(msg)) {
+      return callback('Profanity is not allowed!');
+    }
+
     io.emit('message chat', msg);
+    callback();
+  });
+
+  socket.on('send location', (coords, callback) => {
+    io.emit('message chat', `https://www.google.com/maps?q=${coords.latitude},${coords.longitude}`);
+
+    callback();
   });
 
   socket.on('disconnect', () => {
     io.emit('message', 'a user has left!');
-  })
+  });
 })
 
 server.listen(port, () => {
   console.log(`Server is up on port ${port}!`)
-})
+});
